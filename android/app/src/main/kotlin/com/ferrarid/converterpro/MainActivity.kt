7270fbd2b5c50ac94b5394e310ebd6a1ec40a62b
Damiano <damianoferrari1998@gmail.com>
2020-04-18 21:24:46 +0200

    Added favicon + PWA

---
+++ android/app/src/main/kotlin/com/ferrarid/converterpro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ferrarid.converterpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
