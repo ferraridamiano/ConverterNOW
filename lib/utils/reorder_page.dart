cf150a931aedf5190fcc0de710c44d8774072636
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-04-04 18:25:21 +0200

    ♻️ Refactor `ReorderPage`

--- lib/utils/reorder_page.dart
+++ lib/utils/reorder_page.dart
@@ -30,6 +30,7 @@ class _ReorderPageState extends State<ReorderPage> {
     for (int i = 0; i < widget.itemsList.length; i++) {
       _itemsList.add(Item(i, widget.itemsList[i]));
     }
+    final bool isMobileDevice = !kIsWeb && (Platform.isIOS || Platform.isAndroid);
 
     return Scaffold(
       key: _scaffoldKey,
@@ -57,62 +58,43 @@ class _ReorderPageState extends State<ReorderPage> {
         children: [
           widget.header != null ? widget.header! : const SizedBox(),
           Expanded(
-            child: ReorderList(
-              _itemsList,
-              bottomPadding: 60,
+            child: StatefulBuilder(
+              builder: (context, setState) => Container(
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: ReorderableListView(
+                  scrollController: ScrollController(),
+                  padding: const EdgeInsets.only(bottom: 60),
+                  onReorder: (int oldIndex, int newIndex) {
+                    setState(() => _updateItemsOrder(oldIndex, newIndex));
+                  },
+                  children: List.generate(
+                    widget.itemsList.length,
+                    (index) {
+                      return ListTile(
+                        key: ValueKey(_itemsList[index].id),
+                        title: Center(
+                          child: Text(
+                            _itemsList[index].title,
+                            style: const TextStyle(fontSize: singlePageTextSize),
+                          ),
+                        ),
+                        onTap: isMobileDevice
+                            ? () {
+                                final snackBar = SnackBar(
+                                  content: Text(AppLocalizations.of(context)!.longPressAdvice),
+                                  behavior: SnackBarBehavior.floating,
+                                );
+                                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                              }
+                            : null,
+                      );
+                    },
+                  ),
+                ),
+              ),
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ReorderList extends StatefulWidget {
-  const ReorderList(this.itemsList, {this.bottomPadding = 0, Key? key}) : super(key: key);
-
-  final List<Item> itemsList;
-  final double bottomPadding;
-
-  @override
-  _ReorderListState createState() => _ReorderListState();
-}
-
-class _ReorderListState extends State<ReorderList> {
-  @override
-  Widget build(BuildContext context) {
-    final bool isMobileDevice = !kIsWeb && (Platform.isIOS || Platform.isAndroid);
-    return Container(
-      constraints: const BoxConstraints(maxWidth: 800),
-      child: ReorderableListView(
-        scrollController: ScrollController(),
-        padding: EdgeInsets.only(bottom: widget.bottomPadding),
-        onReorder: (int oldIndex, int newIndex) {
-          setState(() => _updateItemsOrder(oldIndex, newIndex));
-        },
-        children: List.generate(
-          widget.itemsList.length,
-          (index) {
-            return ListTile(
-              key: ValueKey(widget.itemsList[index].id),
-              title: Center(
-                child: Text(
-                  widget.itemsList[index].title,
-                  style: const TextStyle(fontSize: singlePageTextSize),
-                ),
-              ),
-              onTap: isMobileDevice
-                  ? () {
-                      final snackBar = SnackBar(
-                        content: Text(AppLocalizations.of(context)!.longPressAdvice),
-                        behavior: SnackBarBehavior.floating,
-                      );
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                  : null,
-            );
-          },
-        ),
       ),
     );
   }
@@ -121,8 +103,8 @@ class _ReorderListState extends State<ReorderList> {
     if (newIndex > oldIndex) {
       newIndex -= 1;
     }
-    final Item item = widget.itemsList.removeAt(oldIndex);
-    widget.itemsList.insert(newIndex, item);
+    final Item item = _itemsList.removeAt(oldIndex);
+    _itemsList.insert(newIndex, item);
   }
 }
 
