f10c2e6f3746712057582d95e45cec754037fe80
Damiano Ferrari <34270884+ferraridamiano@users.noreply.github.com>
2022-08-23 16:50:01 +0200

    👷 Paths filter (#184)

--- packages/calculator_widget/lib/calculator_model.dart
+++ packages/calculator_widget/lib/calculator_model.dart
@@ -10,7 +10,7 @@ enum OPERATION {
 }
 
 class Calculator with ChangeNotifier {
-  final RegExp _regExpValidatingChar = RegExp(r'^[0-9πe,.+-/=*×÷−]+$'); //da capire se va bene
+  final RegExp _regExpValidatingChar = RegExp(r'^[0-9πe,.+-/=*×÷−]+$');
   final RegExp _regExpNumber = RegExp(r'^[0-9πe]+$');
   static const Map<String, OPERATION> mapOperation = {
     '*': OPERATION.product,
@@ -31,14 +31,15 @@ class Calculator with ChangeNotifier {
 
   Calculator();
 
-  /// With this function you can subit any char from 0-9, [, . + - * / = backspace canc]
+  /// With this function you can subit any char from 0-9, [, . + - * / =
+  /// backspace canc]
   void submitChar(String char) {
     //if string is not valid then return
     if (!(char.length == 1 && _regExpValidatingChar.hasMatch(char))) {
       return;
     }
 
-    //if it is a number
+    // if it is a number
     if (_regExpNumber.hasMatch(char)) {
       if (isResult) {
         isResult = false;
@@ -54,49 +55,63 @@ class Calculator with ChangeNotifier {
       } else if (char == 'e') {
         currentNumber = math.e.toString();
       } else {
-        //is a number
+        // is a number
         currentNumber += char;
       }
     }
     //if char is a comma or a dot
     else if (char == '.' || char == ',') {
-      //if it is already submitted a decimal value ignore it
+      // if it is already submitted a decimal value ignore it
       if (currentNumber.contains('.') || currentNumber.contains(',')) {
         return;
       }
-      //otherwise
+      // otherwise
       currentNumber += '.'; //append the point at the end
     }
-    //if it is an operator
+    // if it is an operator
     else if (mapOperation.containsKey(char)) {
       if (isResult) {
         isResult = false;
         selectedOperation = _firstNumber = _secondNumber = null;
       }
-      //if it is the first operation submitted
-      if (currentNumber.isNotEmpty && _firstNumber == null && selectedOperation == null) {
+      // if it is the first operation submitted
+      if (currentNumber.isNotEmpty &&
+          _firstNumber == null &&
+          selectedOperation == null) {
         _firstNumber = Decimal.parse(currentNumber);
         selectedOperation = mapOperation[char];
         endNumber = true;
-      } else if (currentNumber.isNotEmpty && _firstNumber != null && selectedOperation != null && !endNumber) {
-        //chained operation
+      } else if (currentNumber.isNotEmpty &&
+          _firstNumber != null &&
+          selectedOperation != null &&
+          !endNumber) {
+        // chained operation
         // Compute the result with the previous operator
         _secondNumber = Decimal.parse(currentNumber);
         _computeResult();
         endNumber = true;
         selectedOperation = mapOperation[char];
-      } else if (currentNumber.isNotEmpty && _firstNumber != null && selectedOperation != null) {
-        //change of operation
+      } else if (currentNumber.isNotEmpty &&
+          _firstNumber != null &&
+          selectedOperation != null) {
+        // change of operation
         selectedOperation = mapOperation[char];
       }
     }
     // if it is equal symbol
     else if (char == '=') {
-      if (_firstNumber != null && currentNumber.isNotEmpty && selectedOperation != null && !isResult) {
+      if (_firstNumber != null &&
+          currentNumber.isNotEmpty &&
+          selectedOperation != null &&
+          !isResult) {
         _secondNumber = Decimal.parse(currentNumber);
         _computeResult();
         isResult = true;
-      } else if (_firstNumber != null && currentNumber.isNotEmpty && selectedOperation != null && isResult && _secondNumber != null) {
+      } else if (_firstNumber != null &&
+          currentNumber.isNotEmpty &&
+          selectedOperation != null &&
+          isResult &&
+          _secondNumber != null) {
         _firstNumber = Decimal.parse(currentNumber);
         _computeResult();
       }
@@ -110,18 +125,23 @@ class Calculator with ChangeNotifier {
     }
   }
 
-  ///Returns the symbol of the current operation if an operation is selected otherwise it will return an empty string
+  /// Returns the symbol of the current operation if an operation is selected
+  /// otherwise it will return an empty string
   String get stringOperation {
     if (selectedOperation == null) {
       return '';
     }
-    return mapOperation.keys.where((string) => mapOperation[string] == selectedOperation).last;
+    return mapOperation.keys
+        .where((string) => mapOperation[string] == selectedOperation)
+        .last;
   }
 
-  ///Given firstNumber, secondNumber and selectedOperation in computes the result and put it in currentNumber string
+  /// Given firstNumber, secondNumber and selectedOperation in computes the
+  /// result and put it in currentNumber string
   _computeResult() {
     late Decimal result;
-    assert(_firstNumber != null && _secondNumber != null, 'firstNumber/secondNumber is null');
+    assert(_firstNumber != null && _secondNumber != null,
+        'firstNumber/secondNumber is null');
 
     switch (selectedOperation) {
       case OPERATION.addition:
@@ -134,7 +154,8 @@ class Calculator with ChangeNotifier {
         result = _firstNumber! * _secondNumber!;
         break;
       case OPERATION.division:
-        result = (_firstNumber! / _secondNumber!).toDecimal(scaleOnInfinitePrecision: 15);
+        result = (_firstNumber! / _secondNumber!)
+            .toDecimal(scaleOnInfinitePrecision: 15);
         break;
       case null:
         assert(selectedOperation != null, 'selectedOperation is null');
@@ -144,7 +165,8 @@ class Calculator with ChangeNotifier {
     endNumber = true;
   }
 
-  /// this method bring the calculator to the initial state (nothing submitted, no selected operation)
+  /// This method bring the calculator to the initial state (nothing submitted,
+  /// no selected operation)
   void clearAll() {
     currentNumber = '';
     _firstNumber = null;
@@ -155,7 +177,7 @@ class Calculator with ChangeNotifier {
     notifyListeners();
   }
 
-  ///this method delete the last character of currentNumber
+  /// This method delete the last character of currentNumber
   void deleteLastChar() {
     int length = currentNumber.length;
     if (length > 0) {
@@ -164,28 +186,36 @@ class Calculator with ChangeNotifier {
     }
   }
 
-  /// This method either clearAll() or deleteLastChar() depending on the state of the calculator.
+  /// This method either clearAll() or deleteLastChar() depending on the state
+  /// of the calculator.
   void adaptiveDeleteClear() => endNumber ? clearAll() : deleteLastChar();
 
   /// Computes the square root of currentNumber
-  void squareRoot() => _unaryOperation((Decimal x) => _getStringFromNum(math.sqrt(x.toDouble())));
+  void squareRoot() => _unaryOperation(
+      (Decimal x) => _getStringFromNum(math.sqrt(x.toDouble())));
 
   /// Computes the base-10 logarithm of currentNumber
-  void log10() => _unaryOperation((Decimal x) => _getStringFromNum((math.log(x.toDouble()) / math.log(10))));
+  void log10() => _unaryOperation((Decimal x) =>
+      _getStringFromNum((math.log(x.toDouble()) / math.log(10))));
 
   /// Computes the square of currentNumber
-  void square() => _unaryOperation((Decimal x) => _getStringFromDecimal((x * x)));
+  void square() =>
+      _unaryOperation((Decimal x) => _getStringFromDecimal((x * x)));
 
   /// Computes the natural logarithm (base e) of currentNumber
-  void ln() => _unaryOperation((Decimal x) => _getStringFromNum(math.log(x.toDouble())));
+  void ln() =>
+      _unaryOperation((Decimal x) => _getStringFromNum(math.log(x.toDouble())));
 
   /// Computes the reciprocal (multiplicative inverse) of currentNumber
-  void reciprocal() => _unaryOperation((Decimal x) => x.inverse.toDecimal(scaleOnInfinitePrecision: 15).toString());
+  void reciprocal() => _unaryOperation((Decimal x) =>
+      x.inverse.toDecimal(scaleOnInfinitePrecision: 15).toString());
 
   /// Computes the factorial of currentNumber
-  void factorial() => _unaryOperation((Decimal x) => _getStringFromNum(_myFactorialFunction(x.toBigInt().toInt())));
+  void factorial() => _unaryOperation((Decimal x) =>
+      _getStringFromNum(_myFactorialFunction(x.toBigInt().toInt())));
 
-  /// General function that applies to all the unary operations, just pass the function
+  /// General function that applies to all the unary operations, just pass the
+  /// function
   void _unaryOperation(String Function(Decimal) operation) {
     //if it is the first operation submitted
     if (currentNumber.isNotEmpty) {
