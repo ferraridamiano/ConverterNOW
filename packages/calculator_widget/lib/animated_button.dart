6eef43a0663feb69952e2d6990e5150505c18823
Damiano Ferrari <damianoferrari1998@gmail.com>
2023-07-23 21:07:33 +0200

    Fix calculator error on the web

--- packages/calculator_widget/lib/animated_button.dart
+++ packages/calculator_widget/lib/animated_button.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedButton extends StatefulWidget {
@@ -40,7 +41,8 @@ class _AnimatedButtonState extends State<AnimatedButton>
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia) {
+    if (!kIsWeb &&
+        (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia)) {
       _materialStatesController.addListener(() {
         if (_materialStatesController.value.contains(MaterialState.pressed)) {
           _animationController.forward();
